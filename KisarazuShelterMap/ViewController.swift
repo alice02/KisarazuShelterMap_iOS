3f0af644cff8aa1f9d444a7b65fd05f5df07e870
Kouta ASAI <scene.4913@gmail.com>
2016-03-01 00:56:10 +0900

    現在地を中心に表示できるようになりました

--- KisarazuShelterMap/ViewController.swift
+++ KisarazuShelterMap/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 import MapKit
 import CoreLocation
 
-// 現在地の取得は後ほど
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     // MapView
     @IBOutlet weak var mapView: MKMapView!
+    
+    // LocationManager
+    let locationManager = CLLocationManager()
+    
+    // 最後に位置情報を更新した場所
+    var lastLocation: CLLocationCoordinate2D?
+
+    // 避難場所の情報を格納する配列
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,19 +35,66 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         // デリゲートの設定
         mapView.delegate = self
+        locationManager.delegate = self
         
-        let path = NSBundle.mainBundle().pathForResource("hinan", ofType: "csv")!
-        if let data = NSData(contentsOfFile: path) {
-            print(data)
+        // 100m移動したら位置情報を更新する
+        locationManager.distanceFilter = 100.0
+        
+        // 測位の精度を100mとする
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        // 位置情報サービスへの認証状態を取得
+        let status = CLLocationManager.authorizationStatus()
+        
+        // 未認証ならばダイアログを出す
+        if status == CLAuthorizationStatus.NotDetermined {
+            locationManager.requestWhenInUseAuthorization()
         }
         
+        // 即位開始
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // 位置情報サービスへの認証状態がダイアログによって変更されたとき実行
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        switch status {
+        case CLAuthorizationStatus.AuthorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        case CLAuthorizationStatus.AuthorizedAlways:
+            locationManager.startUpdatingLocation()
+        case CLAuthorizationStatus.Denied:
+            print("Denied")
+        case CLAuthorizationStatus.Restricted:
+            print("Restricted")
+        case CLAuthorizationStatus.NotDetermined:
+            print("NotDetermined")
+        }
+    }
+    
+    // 位置情報が更新されたときに実行
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 位置情報の配列から一番最後に取得した位置情報を取得
+        let locations: NSArray = locations as NSArray
+        let location: CLLocation = locations.lastObject as! CLLocation
+        lastLocation = location.coordinate
+        
+        if let location = lastLocation {
+            // 一番最後に取得した位置情報をマップの中心にする
+            let center: CLLocationCoordinate2D = CLLocationCoordinate2DMake(lastLocation!.latitude, lastLocation!.longitude)
+            mapView.setCenterCoordinate(center, animated: true)
+            
+            // 現在地から3km四方で表示する
+            let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(location, 5000, 5000)
+            mapView.setRegion(myRegion, animated: true)
+        }
 
+    }
 
 }
+
 
