7956cdf89bf2915da3c02d885b37329dbfd08824
Kouta ASAI <scene.4913@gmail.com>
2016-02-29 18:00:52 +0900

    MapKitを追加 表示できるようにした

--- KisarazuShelterMap/ViewController.swift
+++ KisarazuShelterMap/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
