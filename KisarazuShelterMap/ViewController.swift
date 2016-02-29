7685a4c48b68328cadbf8ed5c76a82585414fdee
Kouta ASAI <scene.4913@gmail.com>
2016-02-29 17:58:20 +0900

    Initial Commit

---
+++ KisarazuShelterMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KisarazuShelterMap
+//
+//  Created by Kouta on 2016/02/29.
+//  Copyright © 2016年 Kouta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
